251b713338355f4ba8274e218f2bf4425180d72d
maurux01 <maurux01@gmail.com>
2025-07-17 18:02:41 -0500

    Add dashboard plugin configuration with custom header and key mappings in essentials.lua

--- lua/plugins/essentials.lua
+++ lua/plugins/essentials.lua
@@ -1,4 +1,77 @@
 return {
+  -- Dashboard
+  {
+    "nvimdev/dashboard-nvim",
+    event = "VimEnter",
+    config = function()
+      require("dashboard").setup({
+        theme = "doom",
+        config = {
+          header = {
+            "",
+            "",
+            "",
+            "",
+            "",
+            " ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██║ ███╗   ███╗",
+            " ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║",
+            " ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║",
+            " ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║",
+            " ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║",
+            " ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝",
+            "",
+            "",
+          },
+          center = {
+            {
+              icon = "  ",
+              desc = "Find File",
+              group = "Telescope",
+              action = "Telescope find_files",
+              key = "f",
+            },
+            {
+              icon = "  ",
+              desc = "Recent Files",
+              group = "Telescope",
+              action = "Telescope oldfiles",
+              key = "r",
+            },
+            {
+              icon = "  ",
+              desc = "Find Word",
+              group = "Telescope",
+              action = "Telescope live_grep",
+              key = "g",
+            },
+            {
+              icon = "⚙️  ",
+              desc = "Themes",
+              group = "NvChad",
+              action = "Telescope themes",
+              key = "t",
+            },
+            {
+              icon = "  ",
+              desc = "New File",
+              group = "Default",
+              action = ":enew<CR>",
+              key = "e",
+            },
+            {
+              icon = "  ",
+              desc = "Quit",
+              group = "Default",
+              action = "qa",
+              key = "q",
+            },
+          },
+          footer = {},
+        },
+      })
+    end,
+    dependencies = { "nvim-tree/nvim-web-devicons" }
+  },
   -- File Explorer
   {
     "nvim-tree/nvim-tree.lua",
